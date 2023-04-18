dfd2a95d8fe9c1888a625d10fe16a85452297474
Eranmonnie <ajalaferanmi123@gmail.com>
2023-04-18 12:05:40 +0100

    bolean and conditonals

--- index.php
+++ index.php
@@ -3,13 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <style>
+        body{
+            display: grid;
+            place-items:center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
-    <h1>hello world</h1>
+
     <?php 
-    $greeting = "hello";
-    $name = 'world';
-    echo "$greeting  $name";
+    $name = 'dark matter';
+    $read = false;
+    $message = 'you have not read dark matter';
+
+    if($read){
+        $message = 'you have read dark matter';
+
+    }
     ?>
+
+   
+    <h1><?php echo $message; ?></h1>
+   
+  
 </body>
 </html>
