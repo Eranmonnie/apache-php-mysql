ebb02899986d0fe6a93c03f407e5c0613ce3387a
Eranmonnie <ajalaferanmi123@gmail.com>
2023-04-20 10:47:35 +0100

    custom router

--- index.php
+++ index.php
@@ -1,52 +1,13 @@
-<?php 
-$heading= "Homepage";
-$books = [
+<?php
 
-    [
+$uri = $_SERVER["REQUEST_URI"];
 
-    "name"  => "Do androide dream of electric sheep",
-    "author" => "hope vandine",
-    "relese year" => "2004",
-    "url" => "http//:example.com"
-    
-],
-
-[
-    
-    "name" => "Hail mary",
-    "author" => "thomas cruch",
-    "relese year" => "2009",
-    "url" => "http//:example.com"
-],
-
-[
-    
-    "name" => "charlie simson",
-    "author" => "andie strongheart",
-    "relese year" => "20011",
-    "url" => "http//:example.com"
-],
-    
-];
-
-//you can use phps array_filter fuunction it also worka like the filter function we created
-function filter  ($items, $function){
-
-    $filteredItems = [];
-
-    foreach ($items as $item){   
-        if($function($item)){
-            
-            $filteredItems[] = $item;    
-        }                
-    }
-    return $filteredItems; 
-
-    }
-    
-
-$result = filter($books, function($item){
-    return $item['name'] === 'charlie simson';
-});
-
-require "views/index.view.php";
+if ($uri ==="/") {
+    require "controllers/index.php";
+}
+else if ($uri === "/about"){
+    require "controllers/about.php";
+}
+else if ($uri ==="/contact"){
+    require "controllers/contact.php";
+}
--- views/partial/navbar.view.php
+++ views/partial/navbar.view.php
@@ -28,7 +28,7 @@
 <nav>
         <ul class="navstyle">
             <li><a href="/">Home</a></li>
-            <li><a href="/about.php">About us</a></li>
-            <li><a href="/contact.php">Contact us</a></li>
+            <li><a href="/about">About us</a></li>
+            <li><a href="/contact">Contact us</a></li>
         </ul>
     </nav>
