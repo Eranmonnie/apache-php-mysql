2789f5216a5dd0be9f12780aa083f88e29e5a2bd
Eranmonnie <ajalaferanmi123@gmail.com>
2023-04-20 14:07:55 +0100

    made dedicated router for app

--- index.php
+++ index.php
@@ -1,26 +1,2 @@
 <?php
-
-$uri = $_SERVER["REQUEST_URI"];
-
-$uri = parse_url($uri)["path"];
-
-
-$routes =[
-    "/" => "controllers/index.php",
-    "/about" => "controllers/about.php",
-    "/contact" => "controllers/contact.php",
-];
-
-
-if (array_key_exists($uri, $routes)){
-    require $routes[$uri];
-}
-else{
-    http_response_code(404);
-    require "views/404.php";
-}
- 
-
-//get all the routes probably through a function on each rout page that adds the routes to an array
-//loop through each aass array 
-//use the logic above 
+require "router.php";
---
+++ router.php
@@ -0,0 +1,36 @@
+<?php
+
+$uri = $_SERVER["REQUEST_URI"];
+
+$uri = parse_url($uri)["path"];
+
+$routes =[
+    "/" => "controllers/index.php",
+    "/about" => "controllers/about.php",
+    "/contact" => "controllers/contact.php",
+];
+
+
+
+function abort($code = '404'){
+    http_response_code($code);
+    require "views/{$code}.php";
+    die();
+};
+
+
+function routset($uri, $routes){
+if (array_key_exists($uri, $routes)){
+    require $routes[$uri];
+}
+else{
+    abort("404");
+}
+};
+
+
+routset($uri, $routes);
+
+//get all the routes probably through a function on each rout page that adds the routes to an array
+//loop through each aass array 
+//use the logic above 
