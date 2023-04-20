8f6b5910cb5663b8b9288a78a7418745505d1cf8
Eranmonnie <ajalaferanmi123@gmail.com>
2023-04-20 18:31:07 +0100

    Quick itro to classes in php

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
 <?php
 require "router.php";
+
+//connect to our db
+
+class person {
+ 
+    public $name; 
+    public $age; 
+
+    function braethe(){
+        echo"breathing, {$this->name}";
+    }
+};
+
+$feranmi = new person();
+
+$feranmi->name = "Ajala feranmi ";
+$feranmi->age = 19;
+
+
+($feranmi->braethe());
+ 
