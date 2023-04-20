4ce7b6a76ae22ae3657149bcd089c5addbadee5f
Eranmonnie <ajalaferanmi123@gmail.com>
2023-04-20 12:50:48 +0100

    modified router

--- index.php
+++ index.php
@@ -4,12 +4,23 @@ $uri = $_SERVER["REQUEST_URI"];
 
 $uri = parse_url($uri)["path"];
 
-if ($uri ==="/") {
-    require "controllers/index.php";
+
+$routes =[
+    "/" => "controllers/index.php",
+    "/about" => "controllers/about.php",
+    "/contact" => "controllers/contact.php",
+];
+
+
+if (array_key_exists($uri, $routes)){
+    require $routes[$uri];
 }
-else if ($uri === "/about"){
-    require "controllers/about.php";
+else{
+    http_response_code(404);
+    require "views/404.php";
 }
-else if ($uri ==="/contact"){
-    require "controllers/contact.php";
-}
+ 
+
+//get all the routes probably through a function on each rout page that adds the routes to an array
+//loop through each aass array 
+//use the logic above 
