931cd93254ece21e55863806b206cdc637dcfa25
Eranmonnie <ajalaferanmi123@gmail.com>
2023-04-23 20:55:12 +0100

    moved db class to separate file

---
+++ Database.php
@@ -0,0 +1,21 @@
+<?php
+//connect to our db
+//database class
+class Database{
+
+   public $connection;
+
+    public function __construct(){
+        $password = "eranmonnie";
+        $username = "root";
+        $dsn= 'mysql:host=localhost;port=3306;charset=utf8mb4;dbname=phpdb;';
+        $this->connection = new PDO($dsn, $username, $password);
+    }
+    public function query($query){
+        
+        $statement = $this->connection->prepare($query);
+        $statement->execute();
+        return $statement;
+    }
+}
+
