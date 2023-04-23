3861c2ec515602ad60f5cf97b8523aaed5037f98
Eranmonnie <ajalaferanmi123@gmail.com>
2023-04-23 20:10:24 +0100

    comnected msql to php

--- index.php
+++ index.php
@@ -2,22 +2,14 @@
 require "router.php";
 
 //connect to our db
+$password = "eranmonnie";
+$username = "root";
+$dsn= 'mysql:host=localhost;port=3306;charset=utf8mb4;dbname=phpdb;';
 
-class person {
- 
-    public $name; 
-    public $age; 
+$pdo = new PDO($dsn, $username, $password);
 
-    function braethe(){
-        echo"breathing, {$this->name}";
-    }
-};
+$statement = $pdo->prepare("select * from blogs");
+$statement->execute();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+var_dump($posts);
 
-$feranmi = new person();
-
-$feranmi->name = "Ajala feranmi ";
-$feranmi->age = 19;
-
-
-($feranmi->braethe());
- 
